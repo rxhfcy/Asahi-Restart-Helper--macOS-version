a572db668fe321b0b6f6e3aba10e2f97f52a3503
rxhfcy <42669366+rxhfcy@users.noreply.github.com>
2024-04-30 22:25:35 +0300

    Trivial comment tweaks

--- Asahi Restart Helper/AsahiRestartHelperApp.swift
+++ Asahi Restart Helper/AsahiRestartHelperApp.swift
@@ -3,15 +3,16 @@
 
 import SwiftUI
 
-// A simple menubar-icon-only app to help easily restart their Mac into Linux (or technically any other OS).
+// A simple macOS menubar icon app to easily restart your Apple silicon Mac into Linux from macOS (ARM64 Asahi Linux)
+// - (users are also encouraged to install the corresponding Linux version, to easily restart in macOS from Linux)
 
-// The main struct delegates to AppDelegate for handling all events.
+// The main struct delegates to AppDelegate for handling all events
 @main
 struct AsahiRestartHelperApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
-        // The body is an empty settings scene: no "traditional" UI (the menubar icon is the only entry point)
+        // The body is an empty settings scene (the menubar icon is the only entry point i.e. no "traditional" UI)
         Settings {
             EmptyView()
         }
